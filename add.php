286d045d0e3f5406db7b713c6e3eb39978ae8faa
Daffa Rihadatul Fairus <hanura423@gmail.com>
2023-09-29 19:16:38 +0700

    first commit

---
+++ add.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Add data</title>
+  </head>
+  <body>
+    <form action="" method="post">
+      <table border="1" cellpadding="6" cellspacing="0">
+        <tr>
+          <th><label for="">NIM </label></th>
+          <td><input type="text" name="nim" id="" /></td>
+        </tr>
+        <tr>
+          <th><label for="">NAMA</label></th>
+          <td><input type="text" name="nama" id="" /></td>
+        </tr>
+        <tr>
+          <th><label for="">ALAMAT</label></th>
+          <td><input type="text" name="alamat" id="" /></td>
+        </tr>
+        <tr>
+          <th><label for="">EMAIL</label></th>
+          <td><input type="email" name="email" id="" /></td>
+        </tr>
+      </table>
+      <button type="submit">Save</button>
+    </form>
+  </body>
+</html>
